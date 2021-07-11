f8453b308dbb8b6f47ddb38eeda5b014a97ee82e
treda <tomekreda16@gmail.com>
2021-07-11 15:46:16 +0200

    init

--- oauth-rest/oauth-ui-authorization-code-angular/src/main/resources/src/app/app.service.ts
+++ oauth-rest/oauth-ui-authorization-code-angular/src/main/resources/src/app/app.service.ts
@@ -1,41 +1,45 @@
 import {Injectable} from '@angular/core';
-import { Cookie } from 'ng2-cookies';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { Observable } from 'rxjs/Observable';
+import {Cookie} from 'ng2-cookies';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
+import {Observable} from 'rxjs/Observable';
 import 'rxjs/add/operator/catch';
 import 'rxjs/add/operator/map';
- 
+
 export class Foo {
   constructor(
     public id: number,
-    public name: string) { }
-} 
+    public name: string) {
+  }
+}
 
 @Injectable()
 export class AppService {
-   public clientId = 'newClient';
-   public redirectUri = 'http://localhost:8089/';
+  public clientId = 'newClient';
+  public redirectUri = 'http://localhost:8089/';
 
   constructor(
-    private _http: HttpClient){}
+    private _http: HttpClient) {
+  }
 
-  retrieveToken(code){
-    let params = new URLSearchParams();   
-    params.append('grant_type','authorization_code');
+  retrieveToken(code) {
+    let params = new URLSearchParams();
+    params.append('grant_type', 'authorization_code');
     params.append('client_id', this.clientId);
     params.append('client_secret', 'newClientSecret');
     params.append('redirect_uri', this.redirectUri);
-    params.append('code',code);
+    params.append('code', code);
 
+    console.log('aaaa')
+    console.log(params.toString())
     let headers = new HttpHeaders({'Content-type': 'application/x-www-form-urlencoded; charset=utf-8'});
-     this._http.post('http://localhost:8083/auth/realms/baeldung/protocol/openid-connect/token', params.toString(), { headers: headers })
-    .subscribe(
-      data => this.saveToken(data),
-      err => alert('Invalid Credentials')
-    ); 
+    this._http.post('http://localhost:8083/auth/realms/baeldung/protocol/openid-connect/token', params.toString(), {headers: headers})
+      .subscribe(
+        data => this.saveToken(data),
+        err => alert('Invalid Credentials')
+      );
   }
 
-  saveToken(token){
+  saveToken(token) {
     var expireDate = new Date().getTime() + (1000 * token.expires_in);
     Cookie.set("access_token", token.access_token, expireDate);
     Cookie.set("id_token", token.id_token, expireDate);
@@ -43,15 +47,18 @@ export class AppService {
     window.location.href = 'http://localhost:8089';
   }
 
-  getResource(resourceUrl) : Observable<any>{
-    var headers = new HttpHeaders({'Content-type': 'application/x-www-form-urlencoded; charset=utf-8', 'Authorization': 'Bearer '+Cookie.get('access_token')});
-    return this._http.get(resourceUrl, { headers: headers })
-                   .catch((error:any) => Observable.throw(error.json().error || 'Server error'));
+  getResource(resourceUrl): Observable<any> {
+    var headers = new HttpHeaders({
+      'Content-type': 'application/x-www-form-urlencoded; charset=utf-8',
+      'Authorization': 'Bearer ' + Cookie.get('access_token')
+    });
+    return this._http.get(resourceUrl, {headers: headers})
+      .catch((error: any) => Observable.throw(error.json().error || 'Server error'));
   }
 
-  checkCredentials(){
+  checkCredentials() {
     return Cookie.check('access_token');
-  } 
+  }
 
   logout() {
     let token = Cookie.get('id_token');
@@ -62,5 +69,5 @@ export class AppService {
       + "&post_logout_redirect_uri=" + this.redirectUri;
 
     window.location.href = logoutURL;
-  } 
+  }
 }
--- oauth-rest/oauth-ui-authorization-code-angular/src/main/resources/src/app/home.component.ts
+++ oauth-rest/oauth-ui-authorization-code-angular/src/main/resources/src/app/home.component.ts
@@ -1,6 +1,6 @@
 import {Component} from '@angular/core';
 import {AppService} from './app.service'
- 
+
 @Component({
     selector: 'home-header',
     providers: [AppService],
@@ -14,26 +14,30 @@ import {AppService} from './app.service'
     </div>
 </div>`
 })
- 
+
 export class HomeComponent {
      public isLoggedIn = false;
 
     constructor(
         private _service:AppService){}
- 
+
     ngOnInit(){
-        this.isLoggedIn = this._service.checkCredentials();    
+        this.isLoggedIn = this._service.checkCredentials();
         let i = window.location.href.indexOf('code');
+        console.log('init')
         if(!this.isLoggedIn && i != -1){
+          console.log('aaaa')
             this._service.retrieveToken(window.location.href.substring(i + 5));
         }
     }
 
     login() {
-        window.location.href = 'http://localhost:8083/auth/realms/baeldung/protocol/openid-connect/auth?response_type=code&&scope=openid%20write%20read&client_id=' + 
+      console.log('http://localhost:8083/auth/realms/baeldung/protocol/openid-connect/auth?response_type=code&&scope=openid%20write%20read&client_id=' +
+        this._service.clientId + '&redirect_uri='+ this._service.redirectUri)
+        window.location.href = 'http://localhost:8083/auth/realms/baeldung/protocol/openid-connect/auth?response_type=code&&scope=openid%20write%20read&client_id=' +
           this._service.clientId + '&redirect_uri='+ this._service.redirectUri;
     }
- 
+
     logout() {
         this._service.logout();
     }
